7e4010e463f7232785b27bbf0b67b24c9d9911c7
Fredrik Meyer <hrmeyer@gmail.com>
2019-09-03 21:56:23 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Generative art in pure Javascript
+
+## Installation
+
+Run `npm install`, and then `npm start`. The former will download the necessary packages into the `web_modules` folder, and the latter will just serve the local files to `localhost:5000`.
---
+++ index.js
@@ -0,0 +1,48 @@
+import { randomUniform, randomNormal } from '/web_modules/d3-random.js'
+
+const { PI } = Math
+
+const W = 1500
+const H = 1500
+
+const CX = W / 2
+const CY = H / 2
+
+const radius = W / 2 - 100
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = W
+canvas.height = H
+
+function randomPointOnCircle(cx, cy, r, meanAngle) {
+    const theta = randomNormal(meanAngle, 0.35 * PI)()
+    return [
+        CX + r * Math.cos(theta),
+        CY + r * Math.sin(theta)
+    ]
+}
+
+function drawRandomLineOnCircle(cx, cy, r, meanAngle) {
+    const [a, b] = randomPointOnCircle(cx, cy, r, meanAngle)
+    const [x, y] = randomPointOnCircle(cx, cy, r, meanAngle)
+
+    ctx.moveTo(a, b)
+    ctx.lineTo(x, y)
+    ctx.stroke()
+}
+
+function drawCircles() {
+    ctx.lineWidth = 0.05
+    for(let i = 0; i < 1000; i++) {
+        drawRandomLineOnCircle(CX, CY, radius, 0)
+        drawRandomLineOnCircle(CX, CY, radius, PI / 3)
+    }
+}
+
+ctx.fillStyle = 'black'
+ctx.fillRect(0, 0, W, H)
+
+
+ctx.strokeStyle = `rgb(255, 255, 255, 0.1)`
+drawCircles()
