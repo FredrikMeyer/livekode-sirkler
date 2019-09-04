634e6358974fe677bb1c52e1fe4771e81511f5b8
Fredrik Meyer <hrmeyer@gmail.com>
2019-09-04 21:38:31 +0200

    Gjør klar livekoding

--- live_koding_mal.js
+++ live_koding_mal.js
@@ -3,30 +3,46 @@ import { randomNormal } from '/web_modules/d3-random.js'
 const { PI } = Math
 
 // Width and height
-const W = 1500
-const H = 1500
+const W = 600
+const H = 600
 
 // Center
 const [ CX, CY ] = [ W / 2, H / 2]
+const radius = 200
+
+// Initialize canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = W
+canvas.height = H
+
+// Forklar ide!
+
+ctx.fillStyle = 'black'
+ctx.fillRect(0, 0, W, H)
 
 // Function to make random points on a circles
 
-function randomPointOnCircle(cx, cy, r, meanAngle) {
-    
+function randomPointOnCircle(meanAngle) {
 }
+
+/* console.log(randomPointOnCircle(0)) */
 
 // Function to draw a random line on a circle
 
-function drawRandomLineOnCircle(cx, cy, r, meanAngle) {
-    
+ctx.strokeStyle = 'white'
+
+function drawRandomLineOnCircle(meanAngle) {
 }
+
+/* drawRandomLineOnCircle(0) */
+
 
 // Draw many lines
 
 function drawLines() {
-    
 }
 
+drawLines()
 
-ctx.fillStyle = 'black'
-ctx.fillRect(0, 0, W, H)
+
